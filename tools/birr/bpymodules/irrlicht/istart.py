ffcba62fe0014bee7f630cb2e68f3118dcbb7872
pc0der <pc0der@4d35ec14-021f-0410-984d-e1659ed2ff37>
2007-12-16 21:40:42 +0000

    birr updates...

--- tools/birr/bpymodules/irrlicht/istart.py
+++ tools/birr/bpymodules/irrlicht/istart.py
@@ -1,14 +1,80 @@
 import Blender
 from Blender import sys as bsys
 from Blender import Mathutils
+import iGUI
+
+GModules = [iGUI]
 
 
-# Show the wait cursor in blender
-Blender.Window.WaitCursor(1)
+#-----------------------------------------------------------------------------
+#                            G l o b a l  V a r i a b l e s
+#-----------------------------------------------------------------------------
+GRegKey = 'irrexport'
 
+# config options:
+GExportDir = ''
+GObjects = None
+GErrorMsg = None
+GConfirmOverWrite = True
+GVerbose = True
 
+#-----------------------------------------------------------------------------
+#                                l o a d C o n f i g
+#-----------------------------------------------------------------------------
+def loadConfig():
+    # Looking for a saved key in Blender's Registry
+    RegDict = Blender.Registry.GetKey(GRegKey, True)
 
+    if RegDict:
+        try:
+            GExportDir = RegDict['GExportDir']
+            GConfirmOverWrite = RegDict['GConfirmOverWrite']
+            GVerbose = RegDict['GVerbose']
+        except: 
+            print 'Error Loading Default Config Values'
 
-
-def Main():
+#-----------------------------------------------------------------------------
+#                                c r e a t e G U I 
+#-----------------------------------------------------------------------------
+def createGUI():
     pass
+
+def selectFile():
+
+    GObjects = Blender.Object.GetSelected()
+    if len(GObjects) == 0:
+	    GObjects = Blender.Scene.GetCurrent().getChildren()
+    print "GObjects = ", GObjects
+
+    if not GObjects:
+    	Blender.Draw.PupMenu('ERROR: No objects to export.')
+    else:
+	    fname = bsys.makename(ext=".irr")
+	    if GExportDir:
+		    fname = bsys.join(GExportDir, bsys.basename(fname))
+            
+    Blender.Window.FileSelector(fsCallback, "Export Irrlicht", fname)
+
+
+#-----------------------------------------------------------------------------
+#                                    M a i n
+#-----------------------------------------------------------------------------
+def Main():
+    global GModules
+
+    # Show the wait cursor in blender
+    Blender.Window.WaitCursor(1)
+    print 'istart.Main()'
+
+    # reload modules
+    for module in GModules:		  
+        try:
+            reload(module)
+        except NameError:
+            print 'Unable reload module %s' % (module)
+
+    # Load the default/saved configuration values
+    loadConfig()
+    Blender.Draw.Register(iGUI.gui, iGUI.event, iGUI.buttonEvent)
+    Blender.Window.WaitCursor(0)
+    
