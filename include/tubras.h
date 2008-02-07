5e30ebd68946027504da5457896c84f3d014aa67
pc0der <pc0der@4d35ec14-021f-0410-984d-e1659ed2ff37>
2008-02-07 21:53:36 +0000

    zib license update.

--- include/tubras.h
+++ include/tubras.h
@@ -2,7 +2,7 @@
 // This source file is part of the Tubras game engine.
 //
 // Copyright (c) 2006-2008 Tubras Software, Ltd
-// Also see acknowledgements in Readme.html
+// Also see acknowledgements in docs/Readme.html
 //
 // This software is licensed under the zlib/libpng license. See the file
 // "docs/license.html" for detailed information.
