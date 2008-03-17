fb7b702b283c020ea96e3a4deff494eeb9226702
pc0der <pc0der@4d35ec14-021f-0410-984d-e1659ed2ff37>
2008-03-17 14:13:38 +0000

    add support for optional RLE and 24 bit images.

--- trunk/tools/irrb/irrbmodules/iTGAWriter.py
+++ trunk/tools/irrb/irrbmodules/iTGAWriter.py
@@ -26,20 +26,31 @@ import Blender
 #-----------------------------------------------------------------------------
 #                               w r i t e T G A
 #-----------------------------------------------------------------------------
-def writeTGA(bImage,outFilename,callBack=None):
+def writeTGA(bImage,outFilename,RLE=True,callBack=None):
     file=open(outFilename,'wb')
 
     width,height = bImage.size
+    bpp = bImage.depth
+
+    if bpp < 24:
+        file.close()
+        print 'writeTGA only handles 24 or 32 bit images'
+        return
+    
     header = []
     for i in range(18):
         header.append(chr(0))
-    header[2] = chr(2)      # bgra image
+    if RLE:
+        header[2] = chr(10)     # rle bgra image
+    else:
+        header[2] = chr(2)      # bgra image
     header[13] = chr(width / 256)
     header[12] = chr(width % 256)
     header[15] = chr(height / 256)
     header[14] = chr(height % 256)
-    header[16] = chr(32)   # 32 bpp
-    header[17] = chr(03)   # 00vhaaaa - useful alpha data
+    header[16] = chr(bpp)       # 24 or 32 bpp
+    if bpp == 32:
+        header[17] = chr(03)    # 00vhaaaa - useful alpha data
 
     #
     # write header
@@ -51,12 +62,39 @@ def writeTGA(bImage,outFilename,callBack=None):
     # write data
     #
     for y in range(width):
-        if ((y % 10) == 0) and (callBack != None):
+        if (callBack != None) and ((y % 10) == 0):
             callBack(y)
+        run = ''
+        runLength = 1
+        first = True
+        lastPixel = ''
         for x in range(width):
             p = bImage.getPixelI(x,y)   # rgba
-            pixel = chr(p[2]) + chr(p[1]) + chr(p[0]) + chr(p[3])
-            file.write(pixel)
+            pixel = chr(p[2]) + chr(p[1]) + chr(p[0])
+            if bpp == 32:
+                pixel += chr(p[3])
+                
+            if not RLE:
+                file.write(pixel)
+            else:
+                if first:
+                    lastPixel = pixel
+                else:
+                    if (pixel != lastPixel) or (runLength == 128)
+                        packet = chr(128+runLength-1)
+                        rleData = packet + lastPixel
+                        file.write(rleData)
+                        lastPixel = pixel
+                        runLength = 1
+                    else:
+                        runLength += 1
+        #
+        # write last run
+        #
+        if RLE:
+            packet = chr(128+runLength-1)
+            rleData = packet + lastPixel
+            file.write(rleData)
             		
     file.close()
 	
