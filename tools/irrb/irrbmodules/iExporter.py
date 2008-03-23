673b1a9677c11bfc27a669757902a35e70d6d6e1
pc0der <pc0der@4d35ec14-021f-0410-984d-e1659ed2ff37>
2008-03-23 23:13:28 +0000

    ignore meshes with zero vertices.

--- tools/irrb/irrbmodules/iExporter.py
+++ tools/irrb/irrbmodules/iExporter.py
@@ -351,6 +351,10 @@ class Exporter:
 
         alreadyExported = self._hasMeshBeenExported(mesh.name)
 
+        if len(mesh.verts) == 0:
+            debug('ignoring mesh: %s, no vertices' % mesh.name)
+            return
+
         #
         # write scene node data to scene (.irr) file
         #
