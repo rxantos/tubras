3fa90cee5245e05fd283e1ac64b75c4dcd03d682
pc0der <pc0der@4d35ec14-021f-0410-984d-e1659ed2ff37>
2008-07-20 19:53:07 +0000

    prep for shape animimation experimenting.

---
+++ trunk/src/CAnimatedMeshIrr.cpp
@@ -0,0 +1,11 @@
+// Copyright (C) 2002-2008 Nikolaus Gebhardt
+// This file is part of the "Irrlicht Engine".
+// For conditions of distribution and use, see copyright notice in irrlicht.h
+
+#include "IrrCompileConfig.h"
+#ifdef _IRR_COMPILE_WITH_IRR_MESH_LOADER_
+
+#include "CAnimatedMeshIrr.h"
+
+
+#endif
