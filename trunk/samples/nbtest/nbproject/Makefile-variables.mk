16d45348eb90437038c12dfd1555675a6e99ed4b
pc0der <pc0der@4d35ec14-021f-0410-984d-e1659ed2ff37>
2009-04-11 15:44:07 +0000

    Add irrlicht extensions dir & irrlicht "sandbox" app. Resurrected xmlconfig class for irrlicht only applications.

---
+++ trunk/samples/nbtest/nbproject/Makefile-variables.mk
@@ -0,0 +1,56 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Linux-Static-Debug configuration
+CND_PLATFORM_Linux-Static-Debug=
+CND_ARTIFACT_DIR_Linux-Static-Debug=../../bin
+CND_ARTIFACT_NAME_Linux-Static-Debug=nbtest
+CND_ARTIFACT_PATH_Linux-Static-Debug=../../bin/nbtest
+CND_PACKAGE_DIR_Linux-Static-Debug=dist/Linux-Static-Debug//package
+CND_PACKAGE_NAME_Linux-Static-Debug=nbtest.tar
+CND_PACKAGE_PATH_Linux-Static-Debug=dist/Linux-Static-Debug//package/nbtest.tar
+# Linux-Static-Release configuration
+CND_PLATFORM_Linux-Static-Release=
+CND_ARTIFACT_DIR_Linux-Static-Release=../../bin
+CND_ARTIFACT_NAME_Linux-Static-Release=nbtest
+CND_ARTIFACT_PATH_Linux-Static-Release=../../bin/nbtest
+CND_PACKAGE_DIR_Linux-Static-Release=dist/Linux-Static-Release//package
+CND_PACKAGE_NAME_Linux-Static-Release=nbtest.tar
+CND_PACKAGE_PATH_Linux-Static-Release=dist/Linux-Static-Release//package/nbtest.tar
+# Win32-Shared-Debug configuration
+CND_PLATFORM_Win32-Shared-Debug=MinGW-Windows
+CND_ARTIFACT_DIR_Win32-Shared-Debug=../../bin
+CND_ARTIFACT_NAME_Win32-Shared-Debug=nbtest
+CND_ARTIFACT_PATH_Win32-Shared-Debug=../../bin/nbtest
+CND_PACKAGE_DIR_Win32-Shared-Debug=dist/Win32-Shared-Debug/MinGW-Windows/package
+CND_PACKAGE_NAME_Win32-Shared-Debug=nbtest.tar
+CND_PACKAGE_PATH_Win32-Shared-Debug=dist/Win32-Shared-Debug/MinGW-Windows/package/nbtest.tar
+# Win32-Shared-Release configuration
+CND_PLATFORM_Win32-Shared-Release=MinGW-Windows
+CND_ARTIFACT_DIR_Win32-Shared-Release=../../bin
+CND_ARTIFACT_NAME_Win32-Shared-Release=nbtest
+CND_ARTIFACT_PATH_Win32-Shared-Release=../../bin/nbtest
+CND_PACKAGE_DIR_Win32-Shared-Release=dist/Win32-Shared-Release/MinGW-Windows/package
+CND_PACKAGE_NAME_Win32-Shared-Release=nbtest.tar
+CND_PACKAGE_PATH_Win32-Shared-Release=dist/Win32-Shared-Release/MinGW-Windows/package/nbtest.tar
+# Win32-Static-Debug configuration
+CND_PLATFORM_Win32-Static-Debug=MinGW-Windows
+CND_ARTIFACT_DIR_Win32-Static-Debug=../../bin
+CND_ARTIFACT_NAME_Win32-Static-Debug=nbtest
+CND_ARTIFACT_PATH_Win32-Static-Debug=../../bin/nbtest
+CND_PACKAGE_DIR_Win32-Static-Debug=dist/Win32-Static-Debug/MinGW-Windows/package
+CND_PACKAGE_NAME_Win32-Static-Debug=nbtest.tar
+CND_PACKAGE_PATH_Win32-Static-Debug=dist/Win32-Static-Debug/MinGW-Windows/package/nbtest.tar
+# Win32-Static-Release configuration
+CND_PLATFORM_Win32-Static-Release=MinGW-Windows
+CND_ARTIFACT_DIR_Win32-Static-Release=../../bin
+CND_ARTIFACT_NAME_Win32-Static-Release=nbtest
+CND_ARTIFACT_PATH_Win32-Static-Release=../../bin/nbtest
+CND_PACKAGE_DIR_Win32-Static-Release=dist/Win32-Static-Release/MinGW-Windows/package
+CND_PACKAGE_NAME_Win32-Static-Release=nbtest.tar
+CND_PACKAGE_PATH_Win32-Static-Release=dist/Win32-Static-Release/MinGW-Windows/package/nbtest.tar
---
+++ trunk/samples/sandbox/nbproject/Makefile-variables.mk
@@ -0,0 +1,24 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Win32-Debug configuration
+CND_PLATFORM_Win32-Debug=MinGW-Windows
+CND_ARTIFACT_DIR_Win32-Debug=/c/gdev/tubras/bin
+CND_ARTIFACT_NAME_Win32-Debug=sandbox
+CND_ARTIFACT_PATH_Win32-Debug=/c/gdev/tubras/bin/sandbox
+CND_PACKAGE_DIR_Win32-Debug=dist/Win32-Debug/MinGW-Windows/package
+CND_PACKAGE_NAME_Win32-Debug=sandbox.tar
+CND_PACKAGE_PATH_Win32-Debug=dist/Win32-Debug/MinGW-Windows/package/sandbox.tar
+# Win32-Release configuration
+CND_PLATFORM_Win32-Release=MinGW-Windows
+CND_ARTIFACT_DIR_Win32-Release=/c/gdev/tubras/bin
+CND_ARTIFACT_NAME_Win32-Release=sandbox
+CND_ARTIFACT_PATH_Win32-Release=/c/gdev/tubras/bin/sandbox
+CND_PACKAGE_DIR_Win32-Release=dist/Win32-Release/MinGW-Windows/package
+CND_PACKAGE_NAME_Win32-Release=sandbox.tar
+CND_PACKAGE_PATH_Win32-Release=dist/Win32-Release/MinGW-Windows/package/sandbox.tar
