11dc0f727cc7c60ba09c55fe1e7cbb871aa9f2d4
pc0der <pc0der@4d35ec14-021f-0410-984d-e1659ed2ff37>
2009-04-29 22:06:12 +0000

    NB 6.7 beta update.

--- tools/irrlicht/nbproject/Makefile-variables.mk
+++ tools/irrlicht/nbproject/Makefile-variables.mk
@@ -23,34 +23,34 @@ CND_PACKAGE_DIR_Linux-Static-Release=dist/Linux-Static-Release/GNU-Linux-x86/pac
 CND_PACKAGE_NAME_Linux-Static-Release=irrlicht.tar
 CND_PACKAGE_PATH_Linux-Static-Release=dist/Linux-Static-Release/GNU-Linux-x86/package/irrlicht.tar
 # Win32-Shared-Debug configuration
-CND_PLATFORM_Win32-Shared-Debug=
+CND_PLATFORM_Win32-Shared-Debug=MinGW-Windows
 CND_ARTIFACT_DIR_Win32-Shared-Debug=lib/Win32-gcc
 CND_ARTIFACT_NAME_Win32-Shared-Debug=Irrlicht_d.dll
 CND_ARTIFACT_PATH_Win32-Shared-Debug=lib/Win32-gcc/Irrlicht_d.dll
-CND_PACKAGE_DIR_Win32-Shared-Debug=dist/Win32-Shared-Debug//package
+CND_PACKAGE_DIR_Win32-Shared-Debug=dist/Win32-Shared-Debug/MinGW-Windows/package
 CND_PACKAGE_NAME_Win32-Shared-Debug=libirrlicht.dll.tar
-CND_PACKAGE_PATH_Win32-Shared-Debug=dist/Win32-Shared-Debug//package/libirrlicht.dll.tar
+CND_PACKAGE_PATH_Win32-Shared-Debug=dist/Win32-Shared-Debug/MinGW-Windows/package/libirrlicht.dll.tar
 # Win32-Shared-Release configuration
-CND_PLATFORM_Win32-Shared-Release=
+CND_PLATFORM_Win32-Shared-Release=MinGW-Windows
 CND_ARTIFACT_DIR_Win32-Shared-Release=lib/Win32-gcc
 CND_ARTIFACT_NAME_Win32-Shared-Release=Irrlicht.dll
 CND_ARTIFACT_PATH_Win32-Shared-Release=lib/Win32-gcc/Irrlicht.dll
-CND_PACKAGE_DIR_Win32-Shared-Release=dist/Win32-Shared-Release//package
+CND_PACKAGE_DIR_Win32-Shared-Release=dist/Win32-Shared-Release/MinGW-Windows/package
 CND_PACKAGE_NAME_Win32-Shared-Release=libirrlicht.dll.tar
-CND_PACKAGE_PATH_Win32-Shared-Release=dist/Win32-Shared-Release//package/libirrlicht.dll.tar
+CND_PACKAGE_PATH_Win32-Shared-Release=dist/Win32-Shared-Release/MinGW-Windows/package/libirrlicht.dll.tar
 # Win32-Static-Debug configuration
-CND_PLATFORM_Win32-Static-Debug=
+CND_PLATFORM_Win32-Static-Debug=MinGW-Windows
 CND_ARTIFACT_DIR_Win32-Static-Debug=lib/Win32-gcc
 CND_ARTIFACT_NAME_Win32-Static-Debug=Irrlicht_static_d.a
 CND_ARTIFACT_PATH_Win32-Static-Debug=lib/Win32-gcc/Irrlicht_static_d.a
-CND_PACKAGE_DIR_Win32-Static-Debug=dist/Win32-Static-Debug//package
+CND_PACKAGE_DIR_Win32-Static-Debug=dist/Win32-Static-Debug/MinGW-Windows/package
 CND_PACKAGE_NAME_Win32-Static-Debug=irrlicht.tar
-CND_PACKAGE_PATH_Win32-Static-Debug=dist/Win32-Static-Debug//package/irrlicht.tar
+CND_PACKAGE_PATH_Win32-Static-Debug=dist/Win32-Static-Debug/MinGW-Windows/package/irrlicht.tar
 # Win32-Static-Release configuration
-CND_PLATFORM_Win32-Static-Release=
+CND_PLATFORM_Win32-Static-Release=MinGW-Windows
 CND_ARTIFACT_DIR_Win32-Static-Release=lib/Win32-gcc
 CND_ARTIFACT_NAME_Win32-Static-Release=Irrlicht_static.a
 CND_ARTIFACT_PATH_Win32-Static-Release=lib/Win32-gcc/Irrlicht_static.a
-CND_PACKAGE_DIR_Win32-Static-Release=dist/Win32-Static-Release//package
+CND_PACKAGE_DIR_Win32-Static-Release=dist/Win32-Static-Release/MinGW-Windows/package
 CND_PACKAGE_NAME_Win32-Static-Release=irrlicht.tar
-CND_PACKAGE_PATH_Win32-Static-Release=dist/Win32-Static-Release//package/irrlicht.tar
+CND_PACKAGE_PATH_Win32-Static-Release=dist/Win32-Static-Release/MinGW-Windows/package/irrlicht.tar
