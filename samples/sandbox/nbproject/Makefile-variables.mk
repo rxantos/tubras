4fa8d5687416360c1de68109f176458deba5d871
pc0der <pc0der@4d35ec14-021f-0410-984d-e1659ed2ff37>
2009-05-12 18:09:48 +0000

    linux/sandbox updates.

--- samples/sandbox/nbproject/Makefile-variables.mk
+++ samples/sandbox/nbproject/Makefile-variables.mk
@@ -7,26 +7,26 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Win32-Debug configuration
-CND_PLATFORM_Win32-Debug=MinGW-Windows
+CND_PLATFORM_Win32-Debug=
 CND_ARTIFACT_DIR_Win32-Debug=/c/gdev/tubras/bin
 CND_ARTIFACT_NAME_Win32-Debug=sandbox
 CND_ARTIFACT_PATH_Win32-Debug=/c/gdev/tubras/bin/sandbox
-CND_PACKAGE_DIR_Win32-Debug=dist/Win32-Debug/MinGW-Windows/package
+CND_PACKAGE_DIR_Win32-Debug=dist/Win32-Debug//package
 CND_PACKAGE_NAME_Win32-Debug=sandbox.tar
-CND_PACKAGE_PATH_Win32-Debug=dist/Win32-Debug/MinGW-Windows/package/sandbox.tar
+CND_PACKAGE_PATH_Win32-Debug=dist/Win32-Debug//package/sandbox.tar
 # Win32-Release configuration
-CND_PLATFORM_Win32-Release=MinGW-Windows
+CND_PLATFORM_Win32-Release=
 CND_ARTIFACT_DIR_Win32-Release=/c/gdev/tubras/bin
 CND_ARTIFACT_NAME_Win32-Release=sandbox
 CND_ARTIFACT_PATH_Win32-Release=/c/gdev/tubras/bin/sandbox
-CND_PACKAGE_DIR_Win32-Release=dist/Win32-Release/MinGW-Windows/package
+CND_PACKAGE_DIR_Win32-Release=dist/Win32-Release//package
 CND_PACKAGE_NAME_Win32-Release=sandbox.tar
-CND_PACKAGE_PATH_Win32-Release=dist/Win32-Release/MinGW-Windows/package/sandbox.tar
+CND_PACKAGE_PATH_Win32-Release=dist/Win32-Release//package/sandbox.tar
 # Linux-Debug configuration
-CND_PLATFORM_Linux-Debug=
-CND_ARTIFACT_DIR_Linux-Debug=dist/Linux-Debug/
+CND_PLATFORM_Linux-Debug=GNU-Linux-x86
+CND_ARTIFACT_DIR_Linux-Debug=dist/Linux-Debug/GNU-Linux-x86
 CND_ARTIFACT_NAME_Linux-Debug=sandbox
-CND_ARTIFACT_PATH_Linux-Debug=dist/Linux-Debug//sandbox
-CND_PACKAGE_DIR_Linux-Debug=dist/Linux-Debug//package
+CND_ARTIFACT_PATH_Linux-Debug=dist/Linux-Debug/GNU-Linux-x86/sandbox
+CND_PACKAGE_DIR_Linux-Debug=dist/Linux-Debug/GNU-Linux-x86/package
 CND_PACKAGE_NAME_Linux-Debug=sandbox.tar
-CND_PACKAGE_PATH_Linux-Debug=dist/Linux-Debug//package/sandbox.tar
+CND_PACKAGE_PATH_Linux-Debug=dist/Linux-Debug/GNU-Linux-x86/package/sandbox.tar
