c90b7ab9a23f83b3a5ca77422e57cafe866cbf6a
pc0der <pc0der@4d35ec14-021f-0410-984d-e1659ed2ff37>
2009-08-07 19:51:31 +0000

    nb 6.7.1 updates.

--- examples/sandbox/nbproject/Makefile-Win32-Release.mk
+++ examples/sandbox/nbproject/Makefile-Win32-Release.mk
@@ -1,12 +1,80 @@
-.build-conf:
-	@echo Tool collection not found.
-	@echo Please specify existing tool collection in project properties
-	@exit 1
+#
+# Generated Makefile - do not edit!
+#
+# Edit the Makefile in the project folder instead (../Makefile). Each target
+# has a -pre and a -post target defined where you can add customized code.
+#
+# This makefile implements configuration specific macros and targets.
+
+
+# Environment
+MKDIR=mkdir
+CP=cp
+CCADMIN=CCadmin
+RANLIB=ranlib
+CC=gcc
+CCC=g++
+CXX=g++
+FC=
+AS=as
+
+# Macros
+CND_PLATFORM=MinGW-Windows
+CND_CONF=Win32-Release
+CND_DISTDIR=dist
+
+# Include project Makefile
+include Makefile
+
+# Object Directory
+OBJECTDIR=build/${CND_CONF}/${CND_PLATFORM}
+
+# Object Files
+OBJECTFILES= \
+	${OBJECTDIR}/sandbox.o
+
+# C Compiler Flags
+CFLAGS=
+
+# CC Compiler Flags
+CCFLAGS=
+CXXFLAGS=
+
+# Fortran Compiler Flags
+FFLAGS=
+
+# Assembler Flags
+ASFLAGS=
+
+# Link Libraries and Options
+LDLIBSOPTIONS=../../libs/release/libTubras.a ../../deps/irrlicht/lib/Win32-gcc/Irrlicht_static.a ../../deps/irrklang/lib/Win32-gcc/libirrKlang.a -lgdi32 -lopengl32 -lwinmm
+
+# Build Targets
+.build-conf: ${BUILD_SUBPROJECTS}
+	${MAKE}  -f nbproject/Makefile-Win32-Release.mk /c/gdev/tubras/bin/sandbox.exe
+
+/c/gdev/tubras/bin/sandbox.exe: ../../libs/release/libTubras.a
+
+/c/gdev/tubras/bin/sandbox.exe: ../../deps/irrlicht/lib/Win32-gcc/Irrlicht_static.a
+
+/c/gdev/tubras/bin/sandbox.exe: ../../deps/irrklang/lib/Win32-gcc/libirrKlang.a
+
+/c/gdev/tubras/bin/sandbox.exe: ${OBJECTFILES}
+	${MKDIR} -p /c/gdev/tubras/bin
+	${LINK.cc} -o /c/gdev/tubras/bin/sandbox -s ${OBJECTFILES} ${LDLIBSOPTIONS} 
+
+${OBJECTDIR}/sandbox.o: nbproject/Makefile-${CND_CONF}.mk sandbox.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -s -DWIN32 -DNDEBUG -D__GNUWIN32__ -D_IRR_STATIC_LIB_ -DSTATIC_LINKED -I../../include -I../../src/lua -I../../deps/bullet/src -I../../deps/irrlicht/include -I../../deps/irrlicht/source/Irrlicht -I../../deps/irrklang/include -I../../deps/particle2/Particle2/ParticleLib -I../../deps/particle2/Particle2/Particle -MMD -MP -MF $@.d -o ${OBJECTDIR}/sandbox.o sandbox.cpp
+
+# Subprojects
+.build-subprojects:
 
 # Clean Targets
 .clean-conf:
 	${RM} -r build/Win32-Release
-	${RM} /c/gdev/tubras/bin/sandbox
+	${RM} /c/gdev/tubras/bin/sandbox.exe
 
 # Subprojects
 .clean-subprojects:
--- nbproject/Makefile-variables.mk
+++ nbproject/Makefile-variables.mk
@@ -7,26 +7,26 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Linux-Debug configuration
-CND_PLATFORM_Linux-Debug=GNU-Linux-x86
+CND_PLATFORM_Linux-Debug=
 CND_ARTIFACT_DIR_Linux-Debug=libs/debug
 CND_ARTIFACT_NAME_Linux-Debug=libTubras.a
 CND_ARTIFACT_PATH_Linux-Debug=libs/debug/libTubras.a
-CND_PACKAGE_DIR_Linux-Debug=dist/Linux-Debug/GNU-Linux-x86/package
+CND_PACKAGE_DIR_Linux-Debug=dist/Linux-Debug//package
 CND_PACKAGE_NAME_Linux-Debug=tubras.tar
-CND_PACKAGE_PATH_Linux-Debug=dist/Linux-Debug/GNU-Linux-x86/package/tubras.tar
+CND_PACKAGE_PATH_Linux-Debug=dist/Linux-Debug//package/tubras.tar
 # Win32-Debug configuration
-CND_PLATFORM_Win32-Debug=
+CND_PLATFORM_Win32-Debug=MinGW-Windows
 CND_ARTIFACT_DIR_Win32-Debug=libs/debug
 CND_ARTIFACT_NAME_Win32-Debug=libTubras_d.a
 CND_ARTIFACT_PATH_Win32-Debug=libs/debug/libTubras_d.a
-CND_PACKAGE_DIR_Win32-Debug=dist/Win32-Debug//package
+CND_PACKAGE_DIR_Win32-Debug=dist/Win32-Debug/MinGW-Windows/package
 CND_PACKAGE_NAME_Win32-Debug=tubras.tar
-CND_PACKAGE_PATH_Win32-Debug=dist/Win32-Debug//package/tubras.tar
+CND_PACKAGE_PATH_Win32-Debug=dist/Win32-Debug/MinGW-Windows/package/tubras.tar
 # Win32-Release configuration
-CND_PLATFORM_Win32-Release=
+CND_PLATFORM_Win32-Release=MinGW-Windows
 CND_ARTIFACT_DIR_Win32-Release=libs/release
 CND_ARTIFACT_NAME_Win32-Release=libTubras.a
 CND_ARTIFACT_PATH_Win32-Release=libs/release/libTubras.a
-CND_PACKAGE_DIR_Win32-Release=dist/Win32-Release//package
+CND_PACKAGE_DIR_Win32-Release=dist/Win32-Release/MinGW-Windows/package
 CND_PACKAGE_NAME_Win32-Release=tubras.tar
-CND_PACKAGE_PATH_Win32-Release=dist/Win32-Release//package/tubras.tar
+CND_PACKAGE_PATH_Win32-Release=dist/Win32-Release/MinGW-Windows/package/tubras.tar
