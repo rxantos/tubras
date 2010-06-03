ec00611389371e704b02d6a982c3eebe2512e685
pc0der <pc0der@4d35ec14-021f-0410-984d-e1659ed2ff37>
2010-06-03 18:45:23 +0000

    additional evaluators for line, rect, quat, matrix, & dimension.

--- tools/irrlicht/extensions/timing/IEvaluator.h
+++ tools/irrlicht/extensions/timing/IEvaluator.h
@@ -17,70 +17,70 @@ namespace irr
         };
 
 
-        class EvaluatorU8 : public IEvaluator<u8> {
+        class CEvaluatorU8 : public IEvaluator<u8> {
         public:
             void evaluate(const u8& v0, const u8& v1, u8& out, float fraction) {
                 out = v0 + (u8)((v1 - v0) * fraction);
             }    
         };
 
-        class EvaluatorS8 : public IEvaluator<s8> {
+        class CEvaluatorS8 : public IEvaluator<s8> {
         public:
             void evaluate(const s8& v0, const s8& v1, s8& out, float fraction) {
                 out = v0 + (s8)((v1 - v0) * fraction);
             }    
         };
 
-        class EvaluatorU16 : public IEvaluator<u16> {
+        class CEvaluatorU16 : public IEvaluator<u16> {
         public:
             void evaluate(const u16& v0, const u16& v1, u16& out, float fraction) {
                 out = v0 + (u16)((v1 - v0) * fraction);
             }    
         };
 
-        class EvaluatorS16 : public IEvaluator<s16> {
+        class CEvaluatorS16 : public IEvaluator<s16> {
         public:
             void evaluate(const s16& v0, const s16& v1, s16& out, float fraction) {
                 out = v0 + (s16)((v1 - v0) * fraction);
             }    
         };
 
-        class EvaluatorU32 : public IEvaluator<u32> {
+        class CEvaluatorU32 : public IEvaluator<u32> {
         public:
             void evaluate(const u32& v0, const u32& v1, u32& out, float fraction) {
                 out = v0 + (u32)((v1 - v0) * fraction);
             }    
         };
 
-        class EvaluatorS32 : public IEvaluator<s32> {
+        class CEvaluatorS32 : public IEvaluator<s32> {
         public:
             void evaluate(const s32& v0, const s32& v1, s32& out, float fraction) {
                 out = v0 + (s32)((v1 - v0) * fraction);
             }    
         };
 
-        class EvaluatorF32 : public IEvaluator<f32> {
+        class CEvaluatorF32 : public IEvaluator<f32> {
         public:
             void evaluate(const f32& v0, const f32& v1, f32& out, float fraction) {
                 out = v0 + (f32)((v1 - v0) * fraction);
             }    
         };
 
-        class EvaluatorF64 : public IEvaluator<f64> {
+        class CEvaluatorF64 : public IEvaluator<f64> {
         public:
             void evaluate(const f64& v0, const f64& v1, f64& out, float fraction) {
                 out = v0 + (f64)((v1 - v0) * fraction);
             }    
         };
 
-        class EvaluatorSColor : public IEvaluator<SColor> {
+        class CEvaluatorSColor : public IEvaluator<SColor> {
         public:
             void evaluate(const SColor& v0, const SColor& v1, SColor& out, float fraction) {
                 out = v0.getInterpolated(v1, fraction);
             }    
         };
 
-        class EvaluatorVector2df : public IEvaluator<core::vector2d<f32>> {
+        class CEvaluatorVector2df : public IEvaluator<core::vector2d<f32>> {
         public: 
             void evaluate(core::vector2d<f32>& v0, core::vector2d<f32>& v1,
                 core::vector2d<f32>& out, float fraction) {
@@ -89,7 +89,7 @@ namespace irr
             }
         };
 
-        class EvaluatorVector2di : public IEvaluator<core::vector2d<s32>> {
+        class CEvaluatorVector2di : public IEvaluator<core::vector2d<s32>> {
         public: 
             void evaluate(core::vector2d<s32>& v0, core::vector2d<s32>& v1,
                 core::vector2d<s32>& out, float fraction) {
@@ -98,7 +98,7 @@ namespace irr
             }
         };
 
-        class EvaluatorVector3df : public IEvaluator<core::vector3d<f32>> {
+        class CEvaluatorVector3df : public IEvaluator<core::vector3d<f32>> {
         public: 
             void evaluate(core::vector3d<f32>& v0, core::vector3d<f32>& v1,
                 core::vector3d<f32>& out, float fraction) {
@@ -108,7 +108,7 @@ namespace irr
             }
         };
 
-        class EvaluatorVector3di : public IEvaluator<core::vector3d<s32>> {
+        class CEvaluatorVector3di : public IEvaluator<core::vector3d<s32>> {
         public: 
             void evaluate(core::vector3d<s32>& v0, core::vector3d<s32>& v1,
                 core::vector3d<s32>& out, float fraction) {
@@ -117,6 +117,94 @@ namespace irr
                 out.Z = v0.Z + (s32)((v1.Z - v0.Z) * fraction);
             }
         };
+
+        class CEvaluatorLine2df : public IEvaluator<core::line2d<f32>> {
+        public:
+            void evaluate(const core::line2d<f32>& v0, const core::line2d<f32>& v1,
+                core::line2d<f32>& out, float fraction) {
+                out.start.X =  v0.start.X + ((v1.start.X - v0.start.X) * fraction);
+                out.start.Y =  v0.start.Y + ((v1.start.Y - v0.start.Y) * fraction);
+                out.end.X =  v0.end.X + ((v1.end.X - v0.end.X) * fraction);
+                out.end.Y =  v0.end.Y + ((v1.end.Y - v0.end.Y) * fraction);
+            }
+        };
+
+        class CEvaluatorLine2di : public IEvaluator<core::line2d<s32>> {
+        public:
+            void evaluate(const core::line2d<s32>& v0, const core::line2d<s32>& v1,
+                core::line2d<s32>& out, float fraction) {
+                out.start.X =  v0.start.X + (s32)((v1.start.X - v0.start.X) * fraction);
+                out.start.Y =  v0.start.Y + (s32)((v1.start.Y - v0.start.Y) * fraction);
+                out.end.X =  v0.end.X + (s32)((v1.end.X - v0.end.X) * fraction);
+                out.end.Y =  v0.end.Y + (s32)((v1.end.Y - v0.end.Y) * fraction);
+            }
+        };
+
+        class CEvaluatorRectf : public IEvaluator<core::rectf> {
+        public:
+            void evaluate(const core::rectf& v0, const core::rectf& v1,
+                core::rectf& out, float fraction) {
+                out.UpperLeftCorner.X =  v0.UpperLeftCorner.X + ((v1.UpperLeftCorner.X - v0.UpperLeftCorner.X) * fraction);
+                out.UpperLeftCorner.Y =  v0.UpperLeftCorner.Y + ((v1.UpperLeftCorner.Y - v0.UpperLeftCorner.Y) * fraction);
+                out.LowerRightCorner.X =  v0.LowerRightCorner.X + ((v1.LowerRightCorner.X - v0.LowerRightCorner.X) * fraction);
+                out.LowerRightCorner.Y =  v0.LowerRightCorner.Y + ((v1.LowerRightCorner.Y - v0.LowerRightCorner.Y) * fraction);
+            }
+        };
+
+        class CEvaluatorRecti : public IEvaluator<core::recti> {
+        public:
+            void evaluate(const core::recti& v0, const core::recti& v1,
+                core::recti& out, float fraction) {
+                out.UpperLeftCorner.X =  v0.UpperLeftCorner.X + (s32)((v1.UpperLeftCorner.X - v0.UpperLeftCorner.X) * fraction);
+                out.UpperLeftCorner.Y =  v0.UpperLeftCorner.Y + (s32)((v1.UpperLeftCorner.Y - v0.UpperLeftCorner.Y) * fraction);
+                out.LowerRightCorner.X =  v0.LowerRightCorner.X + (s32)((v1.LowerRightCorner.X - v0.LowerRightCorner.X) * fraction);
+                out.LowerRightCorner.Y =  v0.LowerRightCorner.Y + (s32)((v1.LowerRightCorner.Y - v0.LowerRightCorner.Y) * fraction);
+            }
+        };
+
+        class CEvaluatorQuat : public IEvaluator<core::quaternion> {
+        public:
+            void evaluate(const core::quaternion& v0, const core::quaternion& v1,
+                core::quaternion& out, float fraction) {
+                    out.slerp(v0, v1, fraction);
+            }
+        };
+
+        class CEvaluatorMatrix4 : public IEvaluator<core::matrix4> {
+        public:
+            void evaluate(const core::matrix4& v0, const core::matrix4& v1,
+                core::matrix4& out, float fraction) {
+                    out = v0.interpolate(v1, fraction);
+            }
+        };
+
+        class CEvaluatorDimension2df : public IEvaluator<core::dimension2df> {
+        public:
+            void evaluate(const core::dimension2df& v0, const core::dimension2df& v1,
+                core::dimension2df& out, float fraction) {
+                out.Width = v0.Width + ((v1.Width - v0.Width) * fraction);
+                out.Height = v0.Height + ((v1.Height - v0.Height) * fraction);
+            }
+        };
+
+        class CEvaluatorDimension2di : public IEvaluator<core::dimension2di> {
+        public:
+            void evaluate(const core::dimension2di& v0, const core::dimension2di& v1,
+                core::dimension2di& out, float fraction) {
+                out.Width = v0.Width + (s32)((v1.Width - v0.Width) * fraction);
+                out.Height = v0.Height + (s32)((v1.Height - v0.Height) * fraction);
+            }
+        };
+
+        class CEvaluatorDimension2du : public IEvaluator<core::dimension2du> {
+        public:
+            void evaluate(const core::dimension2du& v0, const core::dimension2du& v1,
+                core::dimension2du& out, float fraction) {
+                out.Width = v0.Width + (u32)((v1.Width - v0.Width) * fraction);
+                out.Height = v0.Height + (u32)((v1.Height - v0.Height) * fraction);
+            }
+        };
+
     }
 }
 #endif
