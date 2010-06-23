1c2e5c62d4c8483d803e62e9e1e30bda751f5edf
pc0der <pc0der@4d35ec14-021f-0410-984d-e1659ed2ff37>
2010-06-23 21:42:58 +0000

    irrb 0.6 - scene node animations

--- trunk/tools/blendfiles/26/scripts/list_action_curves.py
+++ trunk/tools/blendfiles/26/scripts/list_action_curves.py
@@ -1,7 +1,10 @@
 import bpy
 
+
 print('[action curves]')
 for action in bpy.data.actions:
     print('action: {0}'.format(action.name))
     for curve in action.fcurves:
         print('   curve: {0}[{1}]'.format(curve.data_path, curve.array_index))
+        for keyframe in curve.keyframe_points:
+            print('      keyframe: ', keyframe.co, keyframe.interpolation)
--- trunk/tools/blendfiles/26/scripts/list_nla_info.py
+++ trunk/tools/blendfiles/26/scripts/list_nla_info.py
@@ -3,7 +3,7 @@
 # a single (if any) "active" action assigned to it. The active action is
 # referenced through "obj.animation_data.action".
 #
-# NLA tracks are made up of 1 or more actions. All NLA tracks associated with
+# NLA tracks are made up of 1 or more "strips". All NLA tracks associated with
 # an object are stored in the "obj.animation_data.nla_tracks" collection.  
 #
 # Each NLA track is composed of 1 or more NLA Strips.  An NLA Strip contains a
@@ -16,7 +16,16 @@ for action in bpy.data.actions:
 	print('action: {0}'.format(action.name))
         
 
-print('[nla tracks]')
-cube = bpy.data.objects['Cube']
-for track in cube.animation_data.nla_tracks:
-    print('[track: {0}]'.format(track.name))
+obj = bpy.context.active_object
+if obj:
+    print('\n[{0} - NLA Tracks]'.format(obj.name))
+    if obj.animation_data:
+        for track in obj.animation_data.nla_tracks:
+            print('[track: {0}]'.format(track.name))
+            for strip in track.strips:
+                print('  Strip["{0}"]'.format(strip.name))
+                print('     Action: {0}'.format(strip.action.name))
+    else:
+        print('No Animation Data For: {0}'.format(obj.name))
+else:
+    print('Active Object is None')
