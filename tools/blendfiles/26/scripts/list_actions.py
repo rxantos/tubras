c5063abd48d192d8b283c30a3ac76a4907f36315
pc0der <pc0der@4d35ec14-021f-0410-984d-e1659ed2ff37>
2010-10-25 02:04:18 +0000

    Moved test scripts into .blend file.

--- tools/blendfiles/26/scripts/list_actions.py
+++
@@ -1,4 +0,0 @@
-import bpy
-print('[actions]')
-for action in bpy.data.actions:
-	print('action: {0}'.format(action.name))
--- tools/blendfiles/26/scripts/list_armatures.py
+++
@@ -1,5 +0,0 @@
-import bpy
-print('[armatures]')
-
-for armature in bpy.data.armatures:
-    print('armatures: {0}'.format(armature.name))
