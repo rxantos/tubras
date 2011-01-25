4c41c51b9dca53d5669c8ac9ae06ff8e1ec10275
pc0der <pc0der@4d35ec14-021f-0410-984d-e1659ed2ff37>
2011-01-25 18:53:32 +0000

    irrb 0.6 - added panel images & htorst updates.

--- doc/htorst/htorst.py
+++ doc/htorst/htorst.py
@@ -2,18 +2,103 @@ import os.path
 import sys
 import os
 
+#-----------------------------------------------------------------------------
+#                                 P a r m
+#-----------------------------------------------------------------------------
+class Parm:
+    def __init__(self, name):
+        self.name = name
+        self.desc = []
+
+    def processLine(self, line):
+        self.desc.append(line)
+
+#-----------------------------------------------------------------------------
+#                               M e t h o d
+#-----------------------------------------------------------------------------
+class Method:
+    def __init__(self, name):
+        self.name = name
+        self.predesc = []
+        self.postdesc = []
+        self.desc = self.predesc
+        self.parms = []
+        self.ret = ''
+        self.curParm = None
+
+    def processLine(self, line):
+        prefix = line[:2]
+        desc = ''
+        if line == '':
+            if self.curParm:
+                self.curParm.processLine(line)
+            else:
+                self.desc.append('')
+        elif prefix == '\\p':
+            if line.find(':'):
+                name, desc = line[3:].split(':')
+
+            self.curParm = Parm(name.strip())
+            self.curParm.processLine(desc.strip())
+            self.parms.append(self.curParm)
+
+        elif prefix == '\\r':
+            print('split:', line)
+            if line.find(':') >= 0:
+                name, desc = line[3:].split(':')
+            else:
+                name = line
+            self.ret = Parm(name)
+            if len(desc):
+                self.ret.processLine(desc.strip())
+            self.curParm = self.ret
+
+#-----------------------------------------------------------------------------
+#                                C l a s s
+#-----------------------------------------------------------------------------
 class Class:
     def __init__(self, name):
         self.name = name
+        self.description = []
         self.methods = []
         self.properties = []
+        self.curMethod = None
 
     def addMethod(self, name):
         pass
+
+    def processLine(self, line):
+        if line[:2] == '::':
+            self.curMethod = Method(line[2:])
+            self.methods.append(self.curMethod)
+        elif self.curMethod:
+            self.curMethod.processLine(line)
+        else:
+            self.description.append(line)
+        pass
+
+#-----------------------------------------------------------------------------
+#                         _ d u m p C l a s s I n f o
+#-----------------------------------------------------------------------------
+def _dumpClassInfo(classes):
+    print('-------------')
+    for c in classes:
+        print('class: {0}'.format(c.name))
+        for m in c.methods:
+            print('   method: {0}'.format(m.name))
+            for p in m.parms:
+                print('       parm: {0}'.format(p.name))
+                if m.ret:
+                    print('        ret: {0}'.format(m.ret.name))
+                else:
+                    print('        ret: None')
+
 #-----------------------------------------------------------------------------
 #                            p r o c e s s F i l e
 #-----------------------------------------------------------------------------
 def processFile(filename, outDirectory):
+
+    classStack = []
 
     if outDirectory[len(outDirectory)-1] != os.sep:
         outDirectory += os.sep
@@ -26,6 +111,7 @@ def processFile(filename, outDirectory):
     if os.path.exists(ofilename):
         os.unlink(ofilename)
 
+    curClass = None
 
     fin = open(filename)
     fout = open(ofilename, 'w')
@@ -35,29 +121,38 @@ def processFile(filename, outDirectory):
         if pos >= 0:
             sline = line[pos+3:].strip()
             print(sline)
-            fout.write(sline)
 
+            # new class definition?
+            if len(sline) > 2 and sline[0] == ':' and sline[1] != ':':
+                curClass = Class(sline[1:])
+                classStack.append(curClass)
+            elif curClass:
+                curClass.processLine(sline)
+
+    fout.close()
     fin.close()
 
-
+    return classStack
 
 #-----------------------------------------------------------------------------
 #                                u s a g e
 #-----------------------------------------------------------------------------
 def usage(verbose=False):
-    print('usage: htorst.py <input header files> <out directory>')
+    print('usage: htorst.py <out directory> <input header files...> ')
     
 #-----------------------------------------------------------------------------
 #                                 m a i n
 #-----------------------------------------------------------------------------
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
+    if len(sys.argv) < 3:
         usage()
         sys.exit(1)
 
-    outDirectory = './'
+    print(os.path.abspath(os.curdir))
+
+    outDirectory = sys.argv[1]
     filenames = []
-    args = sys.argv[1:]
+    args = sys.argv[2:]
     argc = len(args)
     for i in range(argc):
         parm = args[i]
@@ -65,14 +160,8 @@ if __name__ == '__main__':
             print('Input file missing: {0}'.format(parm))
             continue
 
-        if i < argc-1:
-            filenames.append(parm)
-        else:
-            if not os.path.isdir(parm):
-                filenames.append(parm)
-            else:
-                outDirectory = parm
+        filenames.append(parm)
 
     for filename in filenames:
-        processFile(filename, outDirectory)
-
+        classes = processFile(filename, outDirectory)
+        _dumpClassInfo(classes)
--- tools/irrb/doc/interface.rst
+++ tools/irrb/doc/interface.rst
@@ -6,6 +6,10 @@ irrb Interface Overview
 
 The irrb interface consists of four Blender addon panels.
 
+.. image:: images/empty.png 
+   :width: 256
+   :height: 256
+
 Export Panel
 ------------
 
